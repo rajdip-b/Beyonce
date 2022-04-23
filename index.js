a727f562587aae531a341428d7e382d50e23263e
subhajit1237 <babai.dassubhajit@gmail.com>
2022-04-24 00:41:36 +0530

    beyonce complete

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new Discord.Client({
 })
 
 client.slashcommands = new Discord.Collection()
-client.player = new Player(clients,{
+client.player = new Player(client,{
     ytdlOptions: {
         quality: "highestaudio",
         highWaterMark: 1 << 25
